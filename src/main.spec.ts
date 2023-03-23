e8f6508caf73879891ae9775adf6acfb1bab296b
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-03-23 09:11:49 +0530

    fix(test): app listen cli enabled

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,9 +2,13 @@ import request from 'supertest';
 import app from './main';
 
 describe('Hello World API', () => {
-  it('Should GET / 200 OK', async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello world');
+  it('Should GET / 200 OK', (done: jest.DoneCallback) => {
+    request(app)
+      .get('/')
+      .expect(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello world');
+      })
+      .end(done);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import { Config } from './config';
 
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const PORT = Config.port;
 
@@ -13,9 +16,19 @@ app.get('/', (_: Request, res: Response) => {
   res.send('Hello world');
 });
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${PORT}`);
-});
+// Node path to the current file
+const nodePath = resolve(process.argv[1]);
+
+// Module path to the current file
+const modulePath = resolve(fileURLToPath(import.meta.url));
+
+const isCLI = nodePath === modulePath;
+// Only start the server if the script is run directly
+if (isCLI) {
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 export default app;
